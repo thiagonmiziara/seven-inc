9493ab0733a20ecf8e48815a691bafde8fe3a235
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-12 15:14:16 -0300

    melhorias

--- app/controllers/EmployeeController.js
+++ app/controllers/EmployeeController.js
@@ -1,74 +1,83 @@
-const Service = require('../service/EmployeeService');
+const Service = require("../service/EmployeeService");
 
 module.exports = {
     async getAll(req, res) {
-        const employee = await Service.listAllEmployees();
-
-        return res.json(employee);
+        try {
+            const employee = await Service.listAllEmployees();
+            return res.status(202).json(employee);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
     },
 
     async getById(req, res) {
         const id = req.params.id;
+        try {
+            const data = await Service.findEmployeeById(id);
 
-        const data = await Service.findEmployeeById(id);
-
-        if (data === null) {
-            res.status(404).send({
-                message: `Employee with id:${id} not found!`
-            });
+            if (data === null) {
+                res.status(404).send({
+                    message: `Employee with id:${id} not found!`,
+                });
+            }
+            return res.json(data);
+        } catch (error) {
+            return res.status(500).json(error.message);
         }
-
-        return res.json(data);
     },
 
     async save(req, res) {
-        // Validate Name
-        if (!req.body.name) {
-            res.status(400).send({
-                message: "Name can not be empty!"
-            });
-            return;
-        }
-        // Validate Salary
-        if (!req.body.salary) {
-            res.status(400).send({
-                message: "Salary can not be empty!"
-            });
-            return;
-        }
-        // Validate Position
-        if (!req.body.position) {
-            res.status(400).send({
-                message: "Position can not be empty!"
-            });
-            return;
-        }
-        // Validate BornDate
-        if (!req.body.bornDate) {
-            res.status(400).send({
-                message: "BornDate can not be empty!"
-            });
-            return;
-        }
-        // Validate Position
-        if (!req.body.position) {
-            res.status(400).send({
-                message: "Position can not be empty!"
-            });
-            return;
-        }
+        try {
+            // Validate Name
+            if (!req.body.name) {
+                res.status(400).send({
+                    message: "Name can not be empty!",
+                });
+                return;
+            }
+            // Validate Salary
+            if (!req.body.salary) {
+                res.status(400).send({
+                    message: "Salary can not be empty!",
+                });
+                return;
+            }
+            // Validate Position
+            if (!req.body.position) {
+                res.status(400).send({
+                    message: "Position can not be empty!",
+                });
+                return;
+            }
+            // Validate BornDate
+            if (!req.body.bornDate) {
+                res.status(400).send({
+                    message: "BornDate can not be empty!",
+                });
+                return;
+            }
+            // Validate Position
+            if (!req.body.position) {
+                res.status(400).send({
+                    message: "Position can not be empty!",
+                });
+                return;
+            }
 
-        // Create a Employee
-        const employee = {
-            name: req.body.name,
-            salary: req.body.salary,
-            bornDate: req.body.bornDate,
-            position: req.body.position
-        };
+            // Create a Employee
+            const employee = {
+                name: req.body.name,
+                salary: req.body.salary,
+                bornDate: req.body.bornDate,
+                position: req.body.position,
+            };
 
-        const data = await Service.createNewEmployee(employee);
+            const data = await Service.createNewEmployee(employee);
 
-        return res.status(202).json(data);
+            return res.status(202).json(data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
     },
 
     async updateById(req, res) {
@@ -79,25 +88,31 @@ module.exports = {
             name: req.body.name,
             salary: req.body.salary,
             bornDate: req.body.bornDate,
-            position: req.body.position
+            position: req.body.position,
         };
+        try {
+            const data = await Service.updateEmployeeById(id, employee);
 
-        const data = await Service.updateEmployeeById(id, employee);
+            if (data === null) {
+                res.status(404).send({
+                    message: `Employee with id:${id} not found!`,
+                });
+            }
 
-        if (data === null) {
-            res.status(404).send({
-                message: `Employee with id:${id} not found!`
-            });
+            return res.status(202).json(data);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error.message);
         }
-
-        return res.json(data);
     },
 
     deleteById(req, res) {
         const id = req.params.id;
-
-        Service.deleteEmployeeById(id);
-
-        res.status(404).send('Not found');
-    }
+        try {
+            Service.deleteEmployeeById(id);
+            res.status(404).send("Not found");
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    },
 };
